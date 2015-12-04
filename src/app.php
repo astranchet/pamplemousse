32dab22a010258aca7f08d2fa7a3b5492fd031c8
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2015-12-04 20:45:35 +0100

    Add: first view

--- src/app.php
+++ src/app.php
@@ -2,19 +2,23 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 
 $app = new Silex\Application();
 
 /**  Application */
 
 $app->register(new SessionServiceProvider());
+$app->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
 /** Controller */
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function() use($app) {
-    return new Response();
+    return $app['twig']->render('index.twig');
 });
 
 
