ad0361baded48665248e2bbc42141eb03c6582d8
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2015-12-04 21:24:38 +0100

    Add: logs

--- src/app.php
+++ src/app.php
@@ -1,13 +1,18 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 
 $app = new Silex\Application();
 
 /**  Application */
-
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile'    => __DIR__ . '/../log/app.log',
+    'monolog.name'       => 'pamplemousse',
+    'monolog.level'      => 100
+));
 $app->register(new SessionServiceProvider());
 $app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
