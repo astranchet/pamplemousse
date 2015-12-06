170f71286cdd2b99e020b72150ee5ad60bc58d6f
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2015-12-06 21:15:03 +0100

    Mod: generate thumbnail only if needed

--- src/Pamplemousse/Photos/Controller.php
+++ src/Pamplemousse/Photos/Controller.php
@@ -20,19 +20,34 @@ class Controller
      */
     public function thumbnailAction(Application $app, Request $request, $filename, $width, $height)
     {
-        $webDirectory = __DIR__.'/../../../web/';
+        $webDirectory = __DIR__.'/../../../web';
+        $destDirectory = $webDirectory . $app['config']['thumbnail_dir'] . $width . 'x' . $height . DIRECTORY_SEPARATOR;
+
+        $destFile = $destDirectory . $filename;
+        if (file_exists($destFile)) {
+            $thumbnail = imagecreatefromjpeg($destFile);
+
+             ob_start();
+            imagejpeg($thumbnail);
+            $content = ob_get_clean();
+
+            return new Response($content, 200, [
+                'Content-type'        => 'image/jpeg',
+                'Content-Disposition' => sprintf('filename="%s"', $filename),
+            ]);
+        }
 
         $layer = ImageWorkshop::initFromPath($webDirectory . $app['config']['upload_dir'] . $filename);
         $layer->cropMaximumInPixel(0, 0, "MM");
         $layer->resizeInPixel($width, $height);
         $thumbnail = $layer->getResult();
 
-        $destDirectory = $webDirectory . $app['config']['thumbnail_dir'] . DIRECTORY_SEPARATOR . $width . 'x' . $height;
         $createFolders = true;
         $backgroundColor = null;
         $imageQuality = 95;
 
         $layer->save($destDirectory, $filename, $createFolders, $backgroundColor, $imageQuality);
+        $app['monolog']->addDebug(sprintf("Thumbnail generated: %s/%s", $destDirectory, $filename));
 
         ob_start();
         imagejpeg($thumbnail);
