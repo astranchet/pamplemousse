853cf964820a1eb14b40e26aa28a51f291a88c2f
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2016-02-02 14:17:18 +0100

    Mod: use masonry for gallery

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,11 @@
 smoothScroll.init();
+
+var $grid = $('#gallery').masonry({
+  itemSelector: '.image',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
+$grid.imagesLoaded().progress(function() {
+  $grid.masonry('layout');
+});
