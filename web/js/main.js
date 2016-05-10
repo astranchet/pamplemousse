579924807b9d490c50524d61ab659cdac2199563
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2016-05-10 15:13:00 +0200

    Mod: append last pictures

--- web/js/main.js
+++ web/js/main.js
@@ -11,3 +11,14 @@ $grid.imagesLoaded().progress(function() {
 });
 
 $('#home').css({'background-image': 'url(../images/bg' + Math.floor(Math.random()*5) + '.jpg)'});
+
+// Load last images
+$("#load").click(function() {
+  var lastDate = $(".swipe img").last()[0].getAttribute("data-date-taken");
+  $.get("from/"+lastDate, {}, function(data) {
+
+    $("#gallery").append(data);
+    var $content = $(data);
+    $grid.append($content).masonry('appended', $content);
+  });
+});
