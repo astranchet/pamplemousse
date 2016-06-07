8c8f12be4c63a921708e558ac83be7239da527f1
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2016-06-07 17:01:07 +0200

    Add: date validation

--- src/Pamplemousse/Router.php
+++ src/Pamplemousse/Router.php
@@ -5,6 +5,9 @@ use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class Router implements ControllerProviderInterface
 {
     /**
@@ -19,8 +22,19 @@ class Router implements ControllerProviderInterface
         $controllers->get('/', Controller::class . "::indexAction")
             ->bind('index')
             ;
+
+        $checkDate = function (Request $request, Application $app) {
+            $dates = $app['photos']->getDates();
+            if (isset($dates[$request->get('year')])) {
+                if (array_search($request->get('month'), $dates[$request->get('year')])) {
+                    return null;
+                }
+            }
+            return new Response('Bad date', 404);
+        };
         $controllers->get('/date/{year}/{month}', Controller::class . "::byMonthAction")
             ->bind('date')
+            ->before($checkDate)
             ;
 
         $controllers->get('/from/{date}', Controller::class . "::fromAction")
